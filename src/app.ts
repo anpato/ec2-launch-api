1b67c692515c0016570a44861ef5f3f15e46da17
Andre Pato <anpato1994@gmail.com>
2020-04-11 18:14:27 -0400

    add base route

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import * as express from 'express'
+import { Request, Response } from 'express'
 import * as cors from 'cors'
 import * as bodyParser from 'body-parser'
 import Router from './router'
@@ -6,6 +7,7 @@ import * as Logger from 'morgan'
 import * as compression from 'compression'
 import * as helmet from 'helmet'
 const app = express()
+
 app.use(cors())
 app.use(helmet())
 app.use(bodyParser.json())
@@ -13,6 +15,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(Logger('dev'))
 app.use(compression())
 app.use(`/api`, Router)
+app.get('/', (req: Request, res: Response) => res.json({ status: 'Live' }))
 
 app.listen(process.env.PORT || 3000, () => {
   console.log(`App listening on port ${process.env.PORT || 3000}!`)
